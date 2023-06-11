be3cb1ab017f39ec818a7cae663e6c2d5ff075f3
Masud Mukan <mukanmasud@gmail.com>
2023-06-11 11:49:59 +0600

    New branch develop, add DatabaseContext

---
+++ CryptoBank.WebApi/Database/ApplicationDbContext.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace CryptoBank.WebApi.Database;
+
+public class ApplicationDbContext:DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+}
--- CryptoBank.WebApi/Program.cs
+++ CryptoBank.WebApi/Program.cs
@@ -1,4 +1,10 @@
+using CryptoBank.WebApi.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("ApplicationDbContext")));
 
 // Add services to the container.
 
