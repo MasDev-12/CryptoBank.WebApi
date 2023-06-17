143a5c603368c5f1e0ad4b90ee9b4fcd9f75fdff
Masud Mukan <mukanmasud@gmail.com>
2023-06-17 14:59:57 +0600

    Fixed bugs in features/auth part 3

--- CryptoBank.WebApi/Features/Auth/Services/TokenGenerateService.cs
+++ CryptoBank.WebApi/Features/Auth/Services/TokenGenerateService.cs
@@ -24,7 +24,7 @@ public class TokenGenerateService
         var claims = new List<Claim>
             {
                 new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
-                new Claim(ClaimTypes.Email,user.Email),
+                new Claim(ClaimTypes.Email, user.Email),
             };
 
         foreach (var role in user.Roles)
